2b3b87e6fb81b5f90814ae8ec3571fbd12c7f76e
Stephen <raphael.appsoluteperfection@gmail.com>
2016-12-06 08:00:39 -0600

    Long polling and retrieving and logging in for daycare

--- apis/sqs/client.js
+++ apis/sqs/client.js
@@ -3,10 +3,8 @@
     
     let exports = module.exports = {};
 
-    const AWS = require('aws-sdk');
+    const Consumer = require('sqs-consumer');
     const config = require('../../config/config.js');
-    const sqsCommandHandler = require('../../lib/sqs/command-handler.js');
-    const crypto = require("crypto-js");
 
     function getQueueUrl(){
         let awsUserId = config.awsSettings.awsUserId;
@@ -15,33 +13,22 @@
         return `https://sqs.${awsRegion}.amazonaws.com/${awsUserId}/${awsQueueName}`;
     }
 
-    exports.watchForCommand = (sqsCommandHandler) => {
-        var sqs = new AWS.SQS({apiVersion: '2012-11-05', params: {QueueUrl: getQueueUrl()}}); // using url to queue
-        sqs.receiveMessage(function(err,data){
-            if(err) {
-                console.log('error:',"Fail Send Message" + err);
-            } else {
-                let messages = data.Messages;
-                if (messages && messages.length == 1){
-                    let message = messages [0];
-                    let body = message.Body;
-                    let parsed = JSON.parse(body);
-                    sqsCommandHandler(parsed);
-                    let deleteParams = {
-                        ReceiptHandle: message.ReceiptHandle
-                    };
-                    
-                    sqs.deleteMessage(deleteParams, function(err, data){
-                        if (err) {
-                            console.log(err, err.stack); // an error occurred
-                        }
-                        else {
-                            console.log(data);           // successful response
-                        }
-                    });
-                }
+    exports.watchForCommand = (handler) => {
+        var app = Consumer.create({
+            queueUrl: getQueueUrl(),
+            handleMessage: function (message, done) {
+                let body = message.Body;
+                let parsed = JSON.parse(body);
+                handler(parsed);
+                done();
             }
         });
+        
+        app.on('error', function (err) {
+            console.log(err.message);
+        });
+        
+        app.start();
     };
 
 })();
--- app.js
+++ app.js
@@ -5,6 +5,7 @@
     const config = require('./config/config.js');
     const babyLogger = require('./lib/baby-logger.js');
     const dash_button = require('node-dash-button');
+    const sqs = require('./apis/sqs/client.js');
 
     let dashMac = config.dashSettings.dashMacAddress;
     if (dashMac){
@@ -15,7 +16,7 @@
             babyLogger.log();
         });
     }
-    let awsQueue = config.awsSettings.awsQueue;
+    let awsQueue = config.awsSettings.awsQueueName;
     if (awsQueue){
         sqs.watchForCommand(data => {
             if (data.command && data.command.toLowerCase() === 'daycare'){
