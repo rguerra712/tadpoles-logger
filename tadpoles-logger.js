8c07b8e9ea6d71d3c4b694ce607dba41892b37d8
Stephen <raphael.appsoluteperfection@gmail.com>
2017-01-07 22:18:58 -0600

    Changed date format and encoding key

--- tadpoles-logger.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var babyLogger = require('./lib/baby-logger.js');
-    
-exports.handler = (event, context, callback) => {
-    babyLogger.log();
-    callback(null, 'Log created');
-}
